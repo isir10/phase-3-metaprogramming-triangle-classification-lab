c4714e1ca5261fe5b7a1bb03715e5de6c2c58961
isir10 <isir.malak@moringaschool.com>
2022-10-30 18:31:25 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
   # write code here
+  attr_accessor :x,:y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+  def kind
+    raise TriangleError, 'Sum of two sides must be greater than the third.' if x + z < y || y + z < x || x + y < z
+    raise TriangleError, 'Sum of two sides must not be equal to the third.' if x + z == y || y + z == x || x + y == z
+
+    return :equilateral if x == y && x == z 
+    return :isosceles   if x == y || y == z || x == z
+    return :scalene     if x != y && y != z && x != z
+  end
+  class TriangleError < StandardError
+    def message
+      "Triangle Error"
+    end
+  end
 end
+
